fb257ebb257ab0b417f8ee069b57ae7078617831
Amaury Pouly <amaury.pouly@gmail.com>
2013-11-17 19:15:14 +0000

    More work on usb
    Change-Id: I397a1b3d74de057af2650e18229e93920f2e81cc

--- utils/hwstub/stub/usb_drv_synopsys.c
+++ utils/hwstub/stub/usb_drv_synopsys.c
@@ -23,7 +23,6 @@
 #include "memory.h"
 #include "target.h"
 
-#if 1
 #define REG(x)  (*(volatile uint32_t *)(x))
 
 #define USB_DEV     (USB_BASE + 0x800)
@@ -38,12 +37,15 @@
 #define GRSTCTL_CSRST   (1 << 0)
 /* Interrupt Status Register */
 #define GINTSTS     REG(USB_BASE + 0x14)
+#define GINTSTS_RXFLVL  (1 << 4)
 #define GINTSTS_USBRST  (1 << 12)
 #define GINTSTS_ENUMDNE (1 << 13)
 #define GINTSTS_IEPINT  (1 << 18)
 #define GINTSTS_OEPINT  (1 << 19)
 /* Interrupt Mask Register */
 #define GINTMSK     REG(USB_BASE + 0x18)
+/* Receive Status Read and Pop Register */
+#define GRXSTSP     REG(USB_BASE + 0x20)
 /* Device Configuration Register */
 #define DCFG        REG(USB_DEV)
 #define DCFG_DAD_BM     (0x7f << 4)
@@ -87,6 +89,10 @@
 /* Device Input Endpoint Interrupt Register */
 #define DIEPINT(n)  REG(USB_DEV_IEP + 0x8 + (n) * 0x20)
 #define DIEPINT_XFRC    (1 << 0)
+/* Device Endpoint DFIFO */
+#define DFIFO(n)    REG(USB_BASE + (n) * 0x1000)
+
+static uint32_t *ep0_xfer_buf; // current buffer pointer
 
 void usb_drv_set_address(int address)
 {
@@ -118,18 +124,35 @@ void usb_drv_irq(void)
     uint32_t gintsts = GINTSTS;
     if(gintsts & GINTSTS_USBRST)
     {
+        /* reset EP0 state */
+        DIEPSIZ(0) = 0;
+        DIEPCTL(0) = DIEPCTL_USBAEP | DOEPCTL_SNAK;
+        DOEPSIZ(0) = 0;
+        DOEPCTL(0) = DOEPCTL_USBAEP | DOEPCTL_SNAK;
+        /* reset address */
         usb_drv_set_address(0);
     }
     if(gintsts & GINTSTS_ENUMDNE)
     {
-        /* prepare EP0 */
-        DIEPCTL(0) = DIEPCTL_USBAEP;
-        DOEPSIZ(0) = 1 << DOEPSIZ0_STUPCNT_BP | DOEPSIZ0_PKTCNT | 8 << DOEPSIZ0_XFRSIZ_BP;
-        DOEPCTL(0) = DOEPCTL_USBAEP | DOEPCTL_CNAK | DOEPCTL_EPENA;
+    }
+    if(gintsts & GINTSTS_RXFLVL)
+    {
+        usb_drv_exit();
+#if 0
+        uint32_t grxstsp = GRXSTSP;
+        /* extract packet status */
+        unsigned epnum = grxstsp & 0xf;
+        unsigned bcnt = (grxstsp >> 4) & 0x7ff;
+        /* assume interrupt is only enabled at the right time and we are not
+         * writing past the buffer or things like this */
+        bcnt = (bcnt + 3) / 4;
+        while(bcnt-- > 0)
+            *ep0_xfer_buf++ = DFIFO(epnum);
+#endif
     }
     if(gintsts & GINTSTS_OEPINT)
     {
-        usb_drv_exit();
+        //usb_drv_exit();
     }
     if(gintsts & GINTSTS_IEPINT)
     {
@@ -149,7 +172,7 @@ void usb_drv_init(void)
     /* disable irq */
     GAHBCFG &= ~GAHBCFG_GINT;
     /* unmask enum done and reset interrupts */
-    GINTMSK = GINTSTS_ENUMDNE | GINTSTS_USBRST | GINTSTS_IEPINT | GINTSTS_OEPINT;
+    GINTMSK = GINTSTS_ENUMDNE | GINTSTS_USBRST | GINTSTS_IEPINT | GINTSTS_OEPINT | GINTSTS_RXFLVL;
     GINTSTS = 0xffffffff;
     DOEPMSK = DOEPINT_STUP | DOEPINT_XFRC;
     DOEPINT(0) = 0xffffffff;
@@ -172,138 +195,11 @@ void usb_drv_exit(void)
 
 int usb_drv_recv_setup(struct usb_ctrlrequest *req)
 {
+    /* prepare EP0 */
+    DIEPCTL(0) = DIEPCTL_USBAEP;
+    DOEPSIZ(0) = 1 << DOEPSIZ0_STUPCNT_BP | DOEPSIZ0_PKTCNT | 8 << DOEPSIZ0_XFRSIZ_BP;
+    DOEPCTL(0) = DOEPCTL_USBAEP | DOEPCTL_CNAK | DOEPCTL_EPENA;
+    ep0_xfer_buf = (void *)req;
     /* wait for irq */
     while(true);
 }
-#endif
-
-#if 0
-#include "synopsysotg.h"
-
-union usb_ep0_buffer ep0_buffer;
-
-static struct synopsysotg_state state =
-{
-    .endpoints = { {}, {} },
-};
-
-static struct synopsysotg_config config =
-{
-    .core = (struct synopsysotg_core_regs*)USB_BASE,
-    .phy_16bit = true,
-    .phy_ulpi = false,
-    .use_dma = false,
-    .disable_double_buffering = true,
-    .fifosize = 512,
-    .txfifosize = {64, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-};
-
-static struct usb_instance instance =
-{
-    .driver = &synopsysotg_driver,
-    .driver_config = &config,
-    .driver_state = &state,
-    .buffer = &ep0_buffer
-};
-
-void synopsysotg_target_enable_clocks(const struct usb_instance* instance)
-{
-    target_enable_usb_clocks();
-}
-
-void synopsysotg_target_enable_irq(const struct usb_instance* instance)
-{
-    target_enable_usb_irq();
-}
-
-void synopsysotg_target_disable_irq(const struct usb_instance* instance)
-{
-    target_disable_usb_irq();
-}
-
-void synopsysotg_target_clear_irq(const struct usb_instance* instance)
-{
-    target_clear_usb_irq();
-}
-
-void usb_drv_irq()
-{
-    synopsysotg_irq(&instance);
-}
-
-static int speed = 0;
-
-#define EP(x, in) (union usb_endpoint_number){.number = x, .direction = in}
-
-void usb_handle_bus_reset(const struct usb_instance* data, int highspeed)
-{
-    speed = highspeed;
-}
-
-void usb_handle_timeout(const struct usb_instance* data, union usb_endpoint_number epnum, int bytesleft)
-{
-    usb_drv_exit();
-}
-
-void usb_handle_xfer_complete(const struct usb_instance* data, union usb_endpoint_number epnum, int bytesleft)
-{
-    usb_drv_exit();
-}
-
-void usb_handle_setup_received(const struct usb_instance* data, union usb_endpoint_number epnum, int back2back)
-{
-    usb_drv_exit();
-}
-
-void usb_ep0_expect_setup(const struct usb_instance* data)
-{
-    instance.driver->set_stall(data, EP(0, true), 1);
-    // Set up the OUT pipe for the SETUP packet, STALLing everything else.
-    instance.driver->ep0_start_rx(data, 0);
-}
-
-void usb_drv_set_address(int address)
-{
-    instance.driver->set_address(&instance, address);
-}
-
-int usb_drv_send(int endpoint, void* ptr, int length)
-{
-    return -1;
-}
-
-int usb_drv_recv(int endpoint, void* ptr, int length)
-{
-    return -1;
-}
-
-
-int usb_drv_port_speed(void)
-{
-    return speed;
-}
-
-void usb_drv_stall(int endpoint, bool stall, bool in)
-{
-    instance.driver->set_stall(&instance, EP(endpoint, in), stall);
-}
-
-
-void usb_drv_init(void)
-{
-    instance.driver->init(&instance);
-}
-
-void usb_drv_exit(void)
-{
-    target_mdelay(10);
-    volatile uint32_t *reg = (volatile uint32_t *)(USB_BASE + 0x804);
-    *reg |= 2;
-    while(1);
-}
-
-int usb_drv_recv_setup(struct usb_ctrlrequest *req)
-{
-    while(1);
-}
-#endif
